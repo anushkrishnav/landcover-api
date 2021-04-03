a917e2a5e6aff53e9d88491ae099c1deb127e706
anushkrishnav <anush.venkatakrishna@gmail.com>
2021-04-03 20:03:52 +0530

    api almost done

--- api/server.py
+++ api/server.py
@@ -4,6 +4,7 @@ from starlette.responses import RedirectResponse
 import shutil
 from PIL import Image
 from io import BytesIO
+import numpy as np
 app = FastAPI()
 
 def predict():
@@ -23,10 +24,24 @@ async def predict_api(image: UploadFile = File(...)):
     extension = image.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
-    with open("destination.png", "wb") as buffer:
+    with open("api/destination.png", "wb") as buffer:
         shutil.copyfileobj(image.file, buffer)
-    width, height = Image.open('destination.png').size
-    return width*height
+        width, height = Image.open('api/destination.png').size
+        RGBim = Image.open('api/destination.png').convert('RGB')
+        HSVim = RGBim.convert('HSV')
+        RGBna = np.array(RGBim)
+        HSVna = np.array(HSVim)
+        H = HSVna[:,:,0]
+        lo,hi = 100,140
+        lo = int((lo * 255) / 360)
+        hi = int((hi * 255) / 360)
+        
+        green = np.where((H>lo) & (H<hi))
+
+        count = green[0].size
+        risk = (count*100)/(width*height)
+        print("Pixels matched: {}".format(count))
+    return {'risk': risk}
 
 
 if __name__ == "__main__":
--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 url="http://127.0.0.1:8000/predict/image"
 import requests
 
-files = {'image': open('your_image.png', 'rb')}
+files = {'image': open('satellite.png', 'rb')}
 
 response = requests.request("POST", url, files=files)
 print(response.text)
